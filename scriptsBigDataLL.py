d6623db409479af90624a423262ad90b46eef11f
R4venSlayer <126794953+R4venSlayer@users.noreply.github.com>
2023-08-11 20:57:30 -0500

    Add files via upload
    Taller Python Boto3

---
+++ scriptsBigDataLL.py
@@ -0,0 +1,46 @@
+import boto3
+
+#Primer punto 
+print("PRIMER PUNTO")
+s3 = boto3.client('s3')
+response = s3.list_buckets()
+
+print('Existing buckets:')
+for bucket in response['Buckets']:
+    print(f'  {bucket["Name"]}')
+
+print("")
+
+#Segundo punto
+
+print("SEGUNDO PUNTO")
+response_2 = s3.list_objects(Bucket = "khadajhin")
+
+print("nombre del bucket: khadajhin")
+if 'Contents' in response_2:
+    
+    for f in response_2['Contents']:
+        print(f['Key'])
+else:
+    print("No se encuentra ningun archivo")
+
+#Tercer punto
+
+print("TERCER PUNTO")
+response_3 = s3.upload_file("/home/ubuntu/.aws/personajesLoL.txt","yasuojonia","personajesLoL.txt")
+
+response_2 = s3.list_objects(Bucket = "yasuojonia")
+
+print("nombre del bucket: khadajhin")
+if 'Contents' in response_2:
+    
+    for f in response_2['Contents']:
+        print(f['Key'])
+else:
+    print("No se encuentra ningun archivo")
+
+
+#Cuarto punto
+
+print("CUARTO PUNTO")
+response_4 = s3.download_file("yasuojonia", "personajesLoL.txt", "adcarrys.txt")   
